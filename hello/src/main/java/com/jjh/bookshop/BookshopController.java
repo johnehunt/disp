0bb7a31656c5762467126e22717f2cac3283c1c6
John Hunt <john@matraining.com>
2020-01-30 19:59:58 +0000

    Removed unused import

--- hello/src/main/java/com/jjh/bookshop/BookshopController.java
+++ hello/src/main/java/com/jjh/bookshop/BookshopController.java
@@ -2,7 +2,6 @@ package com.jjh.bookshop;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
