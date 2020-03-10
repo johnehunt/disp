10f6b946306bb8270d7fc40e5a1b16590b8065d8
John Hunt <john@matraining.com>
2020-03-10 09:36:31 +0000

    Added test for Java class implementing a task using Mockito

---
+++ simplebpm/src/test/java/uk/ac/uwe/myBPM/simplebpm/CheckWeatherDelegateTest.java
@@ -0,0 +1,34 @@
+package uk.ac.uwe.myBPM.simplebpm;
+
+import static org.junit.Assert.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.junit.Test;
+
+public class CheckWeatherDelegateTest {
+
+	@Test
+	public void test() {
+		// Set up class under test
+		CheckWeatherDelegate delegate = new CheckWeatherDelegate();
+		// Set up mock of Camunda execution environment
+		DelegateExecution mockExecution = mock(DelegateExecution.class);
+		// Run the test
+		try {
+			delegate.execute(mockExecution);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("throw exception: " + e.getMessage());
+		}
+		// Now verify that the mock was called.
+		verify(mockExecution, times(1))
+		    .setVariable(eq("weatherOK"), 
+		    		     any(Boolean.class));
+	}
+
+}
