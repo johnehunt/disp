a72db7e88499110a778c12aed0ded3b3317f5a10
John Hunt <john@matraining.com>
2020-03-10 09:20:01 +0000

    Added back in @Override annotation

--- simplebpm/src/main/java/uk/ac/uwe/myBPM/simplebpm/CheckWeatherDelegate.java
+++ simplebpm/src/main/java/uk/ac/uwe/myBPM/simplebpm/CheckWeatherDelegate.java
@@ -11,6 +11,7 @@ import org.camunda.bpm.engine.delegate.JavaDelegate;
  */
 public class CheckWeatherDelegate implements JavaDelegate {
 
+	@Override
 	public void execute(DelegateExecution execution) throws Exception {
 		Random rand = new Random();
 		execution.setVariable("weatherOK", rand.nextBoolean());
