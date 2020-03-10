f994ec689dbcc03868bbef181b1aa886528810dc
John Hunt <john@matraining.com>
2020-03-10 09:19:37 +0000

    Added comment to class

--- simplebpm/src/main/java/uk/ac/uwe/myBPM/simplebpm/CheckWeatherDelegate.java
+++ simplebpm/src/main/java/uk/ac/uwe/myBPM/simplebpm/CheckWeatherDelegate.java
@@ -5,9 +5,12 @@ import java.util.Random;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
+/**
+ * A Delegate for Camunda
+ *
+ */
 public class CheckWeatherDelegate implements JavaDelegate {
 
-	@Override
 	public void execute(DelegateExecution execution) throws Exception {
 		Random rand = new Random();
 		execution.setVariable("weatherOK", rand.nextBoolean());
